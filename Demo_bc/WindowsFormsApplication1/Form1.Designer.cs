fbac7f3b45a02bed51cf75c71971750b5b8a4d5d
Tra Giang <giang220798@gmail.com>
2020-03-03 09:06:50 +0700

    chua chinh

---
+++ Demo_bc/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_timkiem = new System.Windows.Forms.Button();
+            this.lbl_WebNguon = new System.Windows.Forms.Label();
+            this.lbl_Webtimkiem = new System.Windows.Forms.Label();
+            this.txt_WebTK = new System.Windows.Forms.TextBox();
+            this.txt_TuKhoa = new System.Windows.Forms.TextBox();
+            this.cbl_webnguon = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_KQDiaChi = new System.Windows.Forms.TextBox();
+            this.txt_KQViTri = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_timkiem
+            // 
+            this.btn_timkiem.Location = new System.Drawing.Point(504, 110);
+            this.btn_timkiem.Name = "btn_timkiem";
+            this.btn_timkiem.Size = new System.Drawing.Size(75, 23);
+            this.btn_timkiem.TabIndex = 0;
+            this.btn_timkiem.Text = "Tìm Kiếm";
+            this.btn_timkiem.UseVisualStyleBackColor = true;
+            this.btn_timkiem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_WebNguon
+            // 
+            this.lbl_WebNguon.AutoSize = true;
+            this.lbl_WebNguon.Location = new System.Drawing.Point(13, 23);
+            this.lbl_WebNguon.Name = "lbl_WebNguon";
+            this.lbl_WebNguon.Size = new System.Drawing.Size(65, 13);
+            this.lbl_WebNguon.TabIndex = 1;
+            this.lbl_WebNguon.Text = "Web Nguồn";
+            // 
+            // lbl_Webtimkiem
+            // 
+            this.lbl_Webtimkiem.AutoSize = true;
+            this.lbl_Webtimkiem.Location = new System.Drawing.Point(13, 76);
+            this.lbl_Webtimkiem.Name = "lbl_Webtimkiem";
+            this.lbl_Webtimkiem.Size = new System.Drawing.Size(76, 13);
+            this.lbl_Webtimkiem.TabIndex = 2;
+            this.lbl_Webtimkiem.Text = "Web Tìm Kiếm";
+            // 
+            // txt_WebTK
+            // 
+            this.txt_WebTK.Location = new System.Drawing.Point(108, 69);
+            this.txt_WebTK.Name = "txt_WebTK";
+            this.txt_WebTK.Size = new System.Drawing.Size(360, 20);
+            this.txt_WebTK.TabIndex = 3;
+            // 
+            // txt_TuKhoa
+            // 
+            this.txt_TuKhoa.Location = new System.Drawing.Point(108, 113);
+            this.txt_TuKhoa.Name = "txt_TuKhoa";
+            this.txt_TuKhoa.Size = new System.Drawing.Size(360, 20);
+            this.txt_TuKhoa.TabIndex = 4;
+            // 
+            // cbl_webnguon
+            // 
+            this.cbl_webnguon.FormattingEnabled = true;
+            this.cbl_webnguon.Items.AddRange(new object[] {
+            "https://www.google.com.vn",
+            "https://www.google.com"});
+            this.cbl_webnguon.Location = new System.Drawing.Point(108, 23);
+            this.cbl_webnguon.Name = "cbl_webnguon";
+            this.cbl_webnguon.Size = new System.Drawing.Size(360, 21);
+            this.cbl_webnguon.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Từ khóa";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_KQDiaChi);
+            this.groupBox1.Controls.Add(this.txt_KQViTri);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 180);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ket Qủa";
+            // 
+            // txt_KQDiaChi
+            // 
+            this.txt_KQDiaChi.Location = new System.Drawing.Point(92, 93);
+            this.txt_KQDiaChi.Name = "txt_KQDiaChi";
+            this.txt_KQDiaChi.Size = new System.Drawing.Size(360, 20);
+            this.txt_KQDiaChi.TabIndex = 3;
+            // 
+            // txt_KQViTri
+            // 
+            this.txt_KQViTri.Location = new System.Drawing.Point(92, 39);
+            this.txt_KQViTri.Name = "txt_KQViTri";
+            this.txt_KQViTri.Size = new System.Drawing.Size(360, 20);
+            this.txt_KQViTri.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Địa chỉ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Vị Trí";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 367);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbl_webnguon);
+            this.Controls.Add(this.txt_TuKhoa);
+            this.Controls.Add(this.txt_WebTK);
+            this.Controls.Add(this.lbl_Webtimkiem);
+            this.Controls.Add(this.lbl_WebNguon);
+            this.Controls.Add(this.btn_timkiem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_timkiem;
+        private System.Windows.Forms.Label lbl_WebNguon;
+        private System.Windows.Forms.Label lbl_Webtimkiem;
+        private System.Windows.Forms.TextBox txt_WebTK;
+        private System.Windows.Forms.TextBox txt_TuKhoa;
+        private System.Windows.Forms.ComboBox cbl_webnguon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_KQDiaChi;
+        private System.Windows.Forms.TextBox txt_KQViTri;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Demo_bc/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,264 @@
+﻿//using System;
+//using System.Net;
+//using System.Text.RegularExpressions;
+//using System.Windows.Forms;
+//using System.Web;
+//using System.IO;
+//using System.Text;
+using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+  
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Tukhoa
+        {
+            private string diachi;
+
+            private int vitri;
+
+            public Tukhoa()
+            {
+
+            }
+            public Tukhoa(string diachi, int vitri)
+            {
+                this.Diachi = diachi;
+                this.Vitri = vitri;
+            }
+
+            public string Diachi
+            {
+                get
+                {
+                    return diachi;
+                }
+
+                set
+                {
+                    diachi = value;
+                }
+            }
+
+            public int Vitri
+            {
+                get
+                {
+                    return vitri;
+                }
+
+                set
+                {
+                    vitri = value;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string strUrl = txt_WebTK.Text;
+            //Uri newUri = new Uri("https://google.com.vn");
+            //int I = GetPosition(newUri, txt_TuKhoa.Text);
+            //txt_KQViTri.Text = I.ToString();
+             Tukhoa ketqua = new Tukhoa();
+              ketqua = LayViTri(txt_WebTK.Text, txt_TuKhoa.Text);
+
+              if (ketqua != null)
+              {
+                  txt_KQViTri.Text = string.Format("{0}", ketqua.Vitri);
+
+                string link = ketqua.Diachi;
+                var linkParser = new Regex(@"(?:https?://|www.)S+?&sa", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+               // var linkParser = new Regex(@"(https?://|www.)S+?&sa", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                foreach (Match m in linkParser.Matches(link))
+                    link = m.Value;
+                string pattern = "&sa";
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string ketqua_DC = rgx.Replace(link, replacement);
+                ////textBox2.Text = ketqua_DC;
+                txt_KQDiaChi.Text = link;
+              }
+              else
+              {
+                  txt_KQViTri.Text = "Từ khóa của bạn không nằm trong Top 100";
+                  txt_KQDiaChi.Text = "NaN";
+              }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbl_webnguon.SelectedIndex = 0;
+        }
+         private Tukhoa LayViTri(string Url, string tukhoa_tk)
+         {
+             Tukhoa ketqua= new Tukhoa();
+             string raw = cbl_webnguon.Text + "/search?num=100&q={0}&btnG=Search";//tìm kiếm trên trang mình lựa chọn ở combobox
+             string search = string.Format(raw, System.Uri.EscapeDataString(tukhoa_tk));
+             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(search);// khới tạo HttpWebRequest theo từ khóa 
+             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+             {
+                 using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                 {
+
+                     string html = reader.ReadToEnd();
+                     ketqua = TimViTri(html, Url);// lấy vị trí một trang web tìm đuoẹc với rrang web cần tìm theo thuật toán viết ở dưới
+                     return ketqua;
+                 }
+             }
+
+         }
+         private Tukhoa TimViTri(string html, string url)
+         {
+            int dem = 1;
+             Tukhoa ketqua = new Tukhoa();
+             var Webget = new HtmlWeb();// khởi tạo một đối tượng HtmlWeb của thư viện HtmlAgilityPack
+             var page = Webget.Load("https://google.com.vn");// load trên địa chỉ google
+            page.LoadHtml(html);
+
+             var list = page.DocumentNode.SelectNodes("//a/@href");//("//h3[@class='r']//a");lấy thành phần href của thẻ a trên tất các link mà google tìm đc
+
+
+            int count = list.Count();// dếm tổng số link tìm đc
+             int i = 0;
+         
+            ArrayList ListUrls1 = new ArrayList();
+            ArrayList ListUrls2 = new ArrayList();
+            foreach (var obj in list)
+             {
+                 i++;
+                 if (i > count)
+                 {
+                     break;
+                 }
+                 else
+                 // i = 4;
+                 {
+                    //        var urls = obj.SelectSingleNode(".").Attributes["href"].Value;
+                    //        if (urls.Contains(url))
+                    //        {
+                    //            // lấy link bài viết
+                    //            ketqua.Diachi = urls;
+                    //            // lấy được vị trí
+                    //            ketqua.Vitri = i;
+                    //            return ketqua;
+                    //        }
+
+                    //    }
+                    //}
+                    //return null;
+                    var http = "https://";
+                    var urls = obj.SelectSingleNode(".").Attributes["href"].Value;// coi lại sai lay thong tin ơ đay
+                   // ListUrls2.Add(urls);// them urls vao mang 
+
+                    if (urls.Contains(http))//kiểm tra kink tìm được có chưa địa chỉ cần tìm k
+                    {
+                        ListUrls1.Add(urls);// them urls vao mang 
+                       // j++;
+                        for (int j = 0; j < ListUrls1.Count; j++)
+                        {
+                            //ListUrls2.Add(ListUrls1[j]);
+                            for (int y = 1; y < ListUrls1.Count; y++)
+                            {
+                                //ListUrls2.Add(ListUrls1[j]);
+                                if (!ListUrls1[j].ToString().Contains(url) && ListUrls1[y].ToString().Contains(url))
+                                {
+                                    ListUrls2.Add(ListUrls1[j]);
+                                }
+                                if (ListUrls1[j].ToString().Contains(url) && ListUrls1[y].ToString().Contains(url))
+                                {
+                                    //ListUrls2.Remove(ListUrls1[j]);
+                                    ListUrls2.Add(ListUrls1[j]);
+                                    //lấy link bài viết
+                                    //ketqua.Diachi = ListUrls1[j].ToString();
+                                    //lấy được vị trí
+                                    ListUrls1.Remove(ListUrls1[y]);
+                                    //ListUrls2.Add(ListUrls1[j]);
+                                    //ketqua.Vitri = j;
+
+
+                                    //ketqua.Vitri = j;
+                                    //ketqua.Vitri = ListUrls2.Count;
+                                    //return ketqua;
+
+                                }
+                               
+
+
+
+                            }
+
+
+                        }
+
+                    }
+                    int j1;
+                    for (j1 = 0; j1 < ListUrls2.Count; j1++)
+                    {
+                        if (ListUrls2[j1].ToString().Contains(url))
+                        {
+                            ketqua.Diachi = ListUrls2[j1].ToString();
+                            ketqua.Vitri = j1+1;
+                            return ketqua;
+                        }
+                    }
+
+                }
+            }
+            return null;
+
+        }
+        //public int GetPosition(Uri url, string searchTerm)
+        //{
+        //    string raw = "http://www.google.com/search?num=39&q={0}&btnG=Search";
+        //    string search = string.Format(raw, HttpUtility.UrlEncode(searchTerm));
+
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(search);
+        //    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        //    {
+        //        using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
+        //        {
+        //            string html = reader.ReadToEnd();
+        //            return SearchGoogle(html, url);
+        //        };
+        //    }
+        //}
+        //public int SearchGoogle(string html, Uri url)
+        //{
+        //    int i;
+        //    string lookup = "(<a class=l href=\")(\\w+[a-zA-Z0-9.-?=/]*)";
+        //    MatchCollection matches = Regex.Matches(html, lookup);
+        //    for (i = 0; i < matches.Count; i++)
+        //    { 
+        //        string match = matches[i].Groups[2].Value;
+        //        if (match.Contains(url.Host))
+        //            return i + 1;
+              
+        //    }
+        //    return i;
+
+        //}
+    }
+}
+
